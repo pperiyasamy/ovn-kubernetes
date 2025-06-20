0837f1bce9b556742029e98bd6c0d924786f84ed
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2025-06-20 12:52:25 +0200

    Allow updating match for a SNAT
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>
    (cherry picked from commit 38a03a464a08ec0276b9383e36c820a00173f8ba)

--- go-controller/pkg/libovsdb/ops/router.go
+++ go-controller/pkg/libovsdb/ops/router.go
@@ -1050,9 +1050,9 @@ func isEquivalentNAT(existing *nbdb.NAT, searched *nbdb.NAT) bool {
 		return false
 	}
 
-	if searched.Match != existing.Match {
+	/*if searched.Match != existing.Match {
 		return false
-	}
+	}*/
 
 	// Compare externalIP if it's not empty.
 	if searched.ExternalIP != "" && searched.ExternalIP != existing.ExternalIP {
